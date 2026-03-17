d1eb8a73dd616c0203288770713c5e97dffe4dfd
Sanket <sanketgaikwad75@gmail.com>
2026-03-17 22:23:44 +0530

    Model Ideation Done

---
+++ src/model.py
@@ -0,0 +1,7 @@
+# Will be building a Encoder only model to train. 
+
+'''
+Steps
+
+Team Features -> Transformer Encoder -> Transformer Embeddings -> Form z using Embeddings -> MLP(z) -> Sigmoid() -> Probability
+'''
